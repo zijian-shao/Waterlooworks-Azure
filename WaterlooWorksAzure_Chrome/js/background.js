8ede1b5bb86383437c06e84acdcbd60405ef7550
Sam Shao <sam.zj.shao@gmail.com>
2018-04-24 23:38:53 -0400

    V1.9.0

--- WaterlooWorksAzure_Chrome/js/background.js
+++ WaterlooWorksAzure_Chrome/js/background.js
@@ -13,6 +13,8 @@ function installWelcome(details) {
 
 /**
  * Update option
+ * @param oldVer
+ * @param newVer
  */
 function updateOptions(oldVer, newVer) {
 
@@ -115,65 +117,110 @@ function versionCompare(v1, v2, options) {
 }
 
 /**
- * Background Started
+ * Toolbar context menu
+ * @param id
+ * @param title
+ * @param contexts
+ * @param onClick
  */
-console.log('Welcome to WaterlooWorks Azure!');
-chrome.runtime.onInstalled.addListener(installWelcome);
+function createContextMenu(id, title, contexts, onClick) {
+    chrome.contextMenus.create({
+        id: id,
+        title: title,
+        contexts: contexts
+    });
 
-var version = getOptionVersion();
-var configs = getOptionListDefault();
-var options;
+    chrome.contextMenus.onClicked.addListener(function (info, tab) {
+        if (info.menuItemId == id) {
+            if (typeof onClick === 'function')
+                onClick();
+        }
+    });
+}
 
-/**
- * Check data updates
- */
-chrome.storage.sync.get(configs, function (items) {
-    options = items;
+function initBackground() {
+    /**
+     * Background Started
+     */
+    console.log('Welcome to WaterlooWorks Azure!');
+    chrome.runtime.onInstalled.addListener(installWelcome);
 
-    // option version
-    if (options.OPT_Version < version)
-        updateOptions(options.OPT_Version, version);
+    var version = getOptionVersion();
+    var configs = getOptionListDefault();
+    var options;
 
-    // extension version
-    if (versionCompare(options.EXT_Version, chrome.app.getDetails().version) < 0)
-        extensionUpdated(options.EXT_Version, chrome.app.getDetails().version);
+    /**
+     * Check data updates
+     */
+    chrome.storage.sync.get(configs, function (items) {
+        options = items;
 
-});
+        // option version
+        if (options.OPT_Version < version)
+            updateOptions(options.OPT_Version, version);
 
-/**
- * Chrome API Calls From Content Scripts
- */
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        // extension version
+        if (versionCompare(options.EXT_Version, chrome.app.getDetails().version) < 0)
+            extensionUpdated(options.EXT_Version, chrome.app.getDetails().version);
 
-    // request = {action: '', data: {type:'', content:''}}
-    var obj = {};
+    });
 
-    // execute script
-    if (request.action == 'executeScript') {
+    /**
+     * Chrome API Calls From Content Scripts
+     */
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
-        if (Array.isArray(request.data)) {
-            for (var i = 0; i < request.data.length; i++) {
-                obj[request.data[i].type] = request.data[i].content;
+        // request = {action: '', data: {type:'', content:''}}
+        var obj = {};
+
+        // execute script
+        if (request.action == 'executeScript') {
+
+            if (Array.isArray(request.data)) {
+                for (var i = 0; i < request.data.length; i++) {
+                    obj[request.data[i].type] = request.data[i].content;
+                }
+            } else {
+                obj[request.data.type] = request.data.content;
             }
-        } else {
-            obj[request.data.type] = request.data.content;
+
+            chrome.tabs.executeScript(sender.tab.id, obj);
+
         }
 
-        chrome.tabs.executeScript(sender.tab.id, obj);
+        // inject css
+        else if (request.action == 'insertCSS') {
 
-    }
+            obj[request.data.type] = request.data.content;
+            chrome.tabs.insertCSS(sender.tab.id, obj);
 
-    // inject css
-    else if (request.action == 'insertCSS') {
+        }
 
-        obj[request.data.type] = request.data.content;
-        chrome.tabs.insertCSS(sender.tab.id, obj);
+        if (typeof sendResponse === 'function') {
+            var response = {'status': 1};
+            sendResponse(response);
+        }
 
-    }
+    });
 
-    if (typeof sendResponse === 'function') {
-        var response = {'status': 1};
-        sendResponse(response);
-    }
+    /**
+     * Add toolbar context menu
+     */
+    createContextMenu('azure-website', chrome.i18n.getMessage('officialWebsite'), ['browser_action'], function () {
+        chrome.tabs.create({
+            'url': 'https://www.zijianshao.com/wwazure/'
+        });
+    });
+    createContextMenu('azure-contribute', chrome.i18n.getMessage('contribute'), ['browser_action'], function () {
+        chrome.tabs.create({
+            'url': 'https://www.paypal.me/zjshao'
+        });
+    });
+    createContextMenu('azure-github', 'GitHub', ['browser_action'], function () {
+        chrome.tabs.create({
+            'url': 'https://github.com/SssWind/Waterlooworks-Azure'
+        });
+    });
+}
 
-});
+initBackground();
