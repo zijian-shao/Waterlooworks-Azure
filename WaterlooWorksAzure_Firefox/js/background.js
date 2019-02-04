aa1681f0ae36ddaf1cd257a767108ef8ab126de3
SssWind <sam.zj.shao@gmail.com>
2019-02-03 20:04:51 -0500

    Firefox V2.1.2

--- WaterlooWorksAzure_Firefox/js/background.js
+++ WaterlooWorksAzure_Firefox/js/background.js
@@ -28,6 +28,45 @@ function updateOptions(oldVer, newVer) {
     console.log('Option version updated!');
 }
 
+function getUninstallLink() {
+    function _getOS() {
+        var OSName = "Unknown";
+        if (window.navigator.userAgent.indexOf("Windows NT 10.0") != -1) OSName = "Windows 10";
+        if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1) OSName = "Windows 8";
+        if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1) OSName = "Windows 7";
+        if (window.navigator.userAgent.indexOf("Windows NT 6.0") != -1) OSName = "Windows Vista";
+        if (window.navigator.userAgent.indexOf("Windows NT 5.1") != -1) OSName = "Windows XP";
+        if (window.navigator.userAgent.indexOf("Windows NT 5.0") != -1) OSName = "Windows 2000";
+        if (window.navigator.userAgent.indexOf("Mac") != -1) OSName = "Mac/iOS";
+        if (window.navigator.userAgent.indexOf("X11") != -1) OSName = "UNIX";
+        if (window.navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
+        return OSName;
+    }
+
+    function _getBrowser() {
+        var ua = navigator.userAgent, tem,
+            M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        if (/trident/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+            return {name: 'IE ', version: (tem[1] || '')};
+        }
+        if (M[1] === 'Chrome') {
+            tem = ua.match(/\bOPR\/(\d+)/);
+            if (tem != null) return {name: 'Opera', version: tem[1]};
+        }
+        M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+        if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+        return {name: M[0], version: M[1]};
+    }
+
+    var urlText = 'https://www.zijianshao.com/wwazure/uninstall/?platform=' + _getBrowser().name.toLowerCase() +
+        '&version=' + encodeURI(browser.runtime.getManifest().version) +
+        '&browser=' + encodeURI(_getBrowser().name + ' ' + _getBrowser().version) +
+        '&os=' + encodeURI(_getOS());
+
+    return urlText;
+}
+
 function createToolbarContextMenu(id, title, contexts, onClick) {
 
     browser.contextMenus.remove(id, function () {
@@ -54,7 +93,7 @@ function initBackground() {
      */
     console.log('Welcome to WaterlooWorks Azure!');
     browser.runtime.onInstalled.addListener(installWelcome);
-    browser.runtime.setUninstallURL("https://www.zijianshao.com/wwazure/uninstall/?platform=firefox", function () {
+    browser.runtime.setUninstallURL(getUninstallLink(), function () {
         if (browser.runtime.lastError) {
         }
     });
