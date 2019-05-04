94f18d4a5a35f4a742a098b45ff12293597e9790
Zijian Shao <sam.zj.shao@gmail.com>
2019-05-04 16:25:52 -0400

    Chrome V2.1.9

--- WaterlooWorksAzure_Chrome/theme/theme_3/functions.js
+++ WaterlooWorksAzure_Chrome/theme/theme_3/functions.js
@@ -1,23 +1,31 @@
 function startTheme() {
 
     // nav icons
-    $('#closeNav ul.nav-list li a').each(function (i, e) {
-        var navText = $(e).text();
+    $('#closeNav .sidebar-nav > ul.nav-list > li > a').each(function (i, e) {
+        var self = $(this);
+        var navText = self.text();
         if (navText.match(/Dashboard/)) {
-            $(e).prepend($('<i class="icon-dashboard"></i>'));
+            self.prepend($('<i class="icon-dashboard"></i>'));
         } else if (navText.match(/Hire Waterloo Co-op/)) {
-            $(e).prepend($('<i class="icon-book"></i>'));
-        } else if (navText.match(/Hire Waterloo/) && !navText.match(/Hire Waterloo Co-op/)) {
-            $(e).prepend($('<i class="icon-briefcase"></i>'));
-        } else if (navText.match(/Appointments/) && !navText.match(/Further/)) {
-            $(e).prepend($('<i class="icon-comment"></i>'));
-        } else if (navText.match(/Appointments/) && navText.match(/Further/)) {
-            $(e).prepend($('<i class="icon-comments"></i>'));
-            $(e).html($(e).html().replace(/ - Further Education/, '<small>Further Education</small>'));
+            self.prepend($('<i class="icon-book"></i>'));
+        } else if (navText.match(/Hire Waterloo(?! Co-op)/)) {
+            self.prepend($('<i class="icon-briefcase"></i>'));
+        } else if (navText.match(/Career Centre Events/)) {
+            self.prepend($('<i class="icon-bell-alt"></i>'));
+        } else if (navText.match(/Appointments/)) {
+            self.prepend($('<i class="icon-comment"></i>'));
+            self.closest('li').find('ul.nav-list.childMenu1 > li > a').each(function (i, e) {
+                var self = $(this);
+                self.text(self.text().replace(/Appointments - /g, ''));
+                self.text(self.text().replace(/ Students/g, ''));
+                self.text(self.text().replace(/ Education/g, ' Edu'));
+            });
         } else if (navText.match(/Help/)) {
-            $(e).prepend($('<i class="icon-info-sign"></i>'));
+            self.prepend($('<i class="icon-info-sign"></i>'));
         } else if (navText.match(/Logout/)) {
-            $(e).prepend($('<i class="icon-signout"></i>'));
+            self.prepend($('<i class="icon-signout"></i>'));
+        } else {
+            self.prepend($('<i class="icon-plus"></i>'));
         }
     });
 
